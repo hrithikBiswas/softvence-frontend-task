91f53c669d4d0dc4f016e5c2d6a650c63c2f4f12
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2026-04-09 13:13:23 +0600

    feat: add dns dependency and configure DNS servers for MongoDB connection
    - Added 'dns' package to dependencies in package.json.
    - Imported 'dns' in mongoDB.js and set custom DNS servers for MongoDB connection.
    - Removed unused proxy.js file which handled authentication routing.

--- src/lib/mongoDB.js
+++ src/lib/mongoDB.js
@@ -1,11 +1,11 @@
-// import dns from 'dns';
+import dns from 'dns';
 import { connect } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
-        // dns.setServers(['1.1.1.1', '8.8.8.8']);
+        dns.setServers(['1.1.1.1', '8.8.8.8']);
         await connect(MONGODB_URI);
         console.log('MongoDb Connected!');
     } catch (error) {
