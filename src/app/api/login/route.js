350071da6c09b8f4c53266d73d8fd35e08317695
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2026-04-09 20:49:59 +0600

    feat: include token in user response after successful login

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -50,6 +50,7 @@ export async function POST(req) {
                 first_name: user.first_name,
                 last_name: user.last_name,
                 email: user.email,
+                token,
             },
         });
 
